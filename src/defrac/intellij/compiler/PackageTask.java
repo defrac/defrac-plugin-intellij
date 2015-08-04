defrac/defrac-plugin-intellij

commit e57465776ab70d29ab202a84cdb609b769f5b242

implemented run action
added defrac run config

fix: run config for android modules
fix: dependent paths in JvmRunningState

src/defrac/intellij/compiler/PackageTask.java
@@ -40,7 +40,7 @@ public final class PackageTask extends BooleanBasedCompilerTask {
 
   @Override
   protected boolean shouldRunForFacet(@NotNull final DefracFacet facet) {
-    return facet.getPlatform() != DefracPlatform.JVM;
+    return facet.getPlatform().isJVM();
   }
 
   @NotNull

resources/META-INF/plugin.xml
@@ -78,6 +78,7 @@
                 factoryClass="defrac.intellij.toolWindow.DefracToolWindowFactory"/>
     <fileTemplateGroup implementation="defrac.intellij.fileTemplate.DefracFileTemplateProvider"/>
     <projectTemplatesFactory implementation="defrac.intellij.projectWizard.DefracProjectTemplatesFactory"/>
+    <runConfigurationProducer implementation="defrac.intellij.run.DefracRunConfigurationProducer" order="first"/>
   </extensions>
 
   <project-components>
@@ -91,6 +92,7 @@
       <implementation-class>defrac.intellij.project.DefracProcess</implementation-class>
     </component>
   </project-components>
+<<<<<<< HEAD
 
   <actions>
     <action id="defrac.action.new.delegate"
@@ -127,5 +129,9 @@
   </actions>
 
   <application-components/>
+=======
+  <actions>
+  </actions>
+>>>>>>> 6fe0cf7... added defrac run config
   <module-components/>
 </idea-plugin>

src/defrac/intellij/DefracBundle.properties
@@ -70,6 +70,7 @@ projectView.source=Source
 
 # project wizard
 projectWizard.error.noPackageName=No package name specified
+projectWizard.error.unqualifiedPackageName=Unqualified package name. Package name must contain at least one '.'
 projectWizard.error.invalidPackageName=Invalid package name
 projectWizard.error.noMainClass=No Main-Class specified
 projectWizard.error.invalidMainClass=Invalid Main-Class

src/defrac/intellij/DefracPlatform.java
@@ -126,6 +126,22 @@ public enum DefracPlatform {
     return this == GENERIC;
   }
 
+  public boolean isAndroid() {
+    return this == ANDROID;
+  }
+
+  public boolean isWeb() {
+    return this == WEB;
+  }
+
+  public boolean isIOS() {
+    return this == IOS;
+  }
+
+  public boolean isJVM() {
+    return this == JVM;
+  }
+
   @Nullable
   public static DefracPlatform byName(@NotNull final String value) {
     return NAME_TO_PLATFORM.get(value);

src/defrac/intellij/compiler/CompileTask.java
@@ -45,7 +45,7 @@ public final class CompileTask extends BooleanBasedCompilerTask {
   }
 
   protected boolean shouldRunForFacet(@NotNull final DefracFacet facet) {
-    return facet.getPlatform() == DefracPlatform.JVM;
+    return facet.getPlatform().isJVM();
   }
 
   @Override

src/defrac/intellij/config/DefracConfig.java
@@ -17,11 +17,15 @@
 package defrac.intellij.config;
 
 import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.google.common.collect.Lists;
 import com.google.common.io.Closeables;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.CachedValueProvider;
@@ -37,6 +41,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
@@ -108,11 +113,16 @@ public final class DefracConfig extends DefracConfigurationBase {
   @Nullable
   public DefracConfigurationBase getPlatform(@NotNull final DefracPlatform platform) {
     switch(platform) {
-      case ANDROID: return android;
-      case GENERIC: return this;
-      case IOS: return ios;
-      case JVM: return jvm;
-      case WEB: return web;
+      case ANDROID:
+        return android;
+      case GENERIC:
+        return this;
+      case IOS:
+        return ios;
+      case JVM:
+        return jvm;
+      case WEB:
+        return web;
     }
 
     throw new IllegalArgumentException("Unknown platform " + platform);
@@ -174,6 +184,15 @@ public final class DefracConfig extends DefracConfigurationBase {
   public DefracConfig setWebSettings(@NotNull final WebSettings value) {
     web = value;
     return this;
+  }
+
+  public void commit(@NotNull final Project project) throws IOException {
+    final File baseDir = VfsUtilCore.virtualToIoFile(project.getBaseDir());
+    final File settingsFile = new File(baseDir, "default.settings");
+
+    write(Suppliers.<OutputStream>ofInstance(new FileOutputStream(settingsFile)));
+
+    LocalFileSystem.getInstance().refreshIoFiles(Collections.singletonList(settingsFile));
   }
 
   public void write(@NotNull final Supplier<OutputStream> supplier) throws IOException {

src/defrac/intellij/ipc/DefracCommands.java
@@ -27,6 +27,7 @@ public final class DefracCommands {
   @NotNull @NonNls public static final String COMPILE_RESULT = "\\[info\\] Compiled (\\d+) units? in (\\d+(s|ms)) \\((\\d+) errors?, (\\d+) warnings?\\)";
 
   @NotNull @NonNls public static final String PACKAGE = "package";
+  @NotNull @NonNls public static final String GEN_MACROS = "gen-macros";
 
   private DefracCommands() {}
 }

src/defrac/intellij/ipc/DefracIpc.java
@@ -119,7 +119,7 @@ public final class DefracIpc implements ProcessListener {
 
   public boolean compile(@Nullable final CompileContext context,
                          @NotNull final DefracPlatform platform) throws IOException, InterruptedException {
-    LOG.info("defrac "+platform.prefixCommand(DefracCommands.COMPILE)+" requested");
+    LOG.info("defrac " + platform.prefixCommand(DefracCommands.COMPILE) + " requested");
 
     return execAndGet(context, new Task<Boolean>() {
       @NotNull
@@ -163,6 +163,62 @@ public final class DefracIpc implements ProcessListener {
 
         LOG.info("Invoking "+platform.prefixCommand(DefracCommands.COMPILE));
         execCommand(context, platform, DefracCommands.COMPILE);
+
+        LOG.info("Waiting for command to complete ...");
+        barrier.await();
+
+        return result.get();
+      }
+    });
+  }
+
+
+  public boolean genMacros(@Nullable final CompileContext context,
+                         @NotNull final DefracPlatform platform) throws IOException, InterruptedException {
+    LOG.info("defrac "+platform.prefixCommand(DefracCommands.GEN_MACROS)+" requested");
+
+    return execAndGet(context, new Task<Boolean>() {
+      @NotNull
+      @Override
+      public Boolean exec() throws IOException, InterruptedException, BrokenBarrierException {
+        if(process.isProcessTerminated() || process.isProcessTerminating()) {
+          addMessage(CompilerMessageCategory.ERROR, "defrac process lost");
+          return false;
+        }
+
+        final CyclicBarrier barrier = new CyclicBarrier(2);
+        final AtomicBoolean result = new AtomicBoolean();
+
+        installParser(new RegExpBasedParser(DefracCommands.COMPILE_RESULT) {
+          @Override
+          protected void onMatch(@NotNull final Matcher matcher) throws InterruptedException, BrokenBarrierException {
+            try {
+              final int errors = Integer.parseInt(matcher.group(4));
+              final boolean isSuccess = errors == 0;
+
+              LOG.info("Gen-macros completed. Result: " + isSuccess);
+              result.set(isSuccess);
+
+              LOG.info("Awaiting barrier ...");
+              barrier.await();
+            } catch(final NumberFormatException exception) {
+              LOG.error(exception);
+              onFailure();
+            }
+          }
+
+          @Override
+          public void onFailure() throws InterruptedException, BrokenBarrierException {
+            LOG.info("Received onFailure callback");
+            result.set(false);
+
+            LOG.info("Awaiting barrier ...");
+            barrier.await();
+          }
+        });
+
+        LOG.info("Invoking "+platform.prefixCommand(DefracCommands.GEN_MACROS));
+        execCommand(context, platform, DefracCommands.GEN_MACROS);
 
         LOG.info("Waiting for command to complete ...");
         barrier.await();

commit 089acd05447801bc977f2b15e56e50629ab4fa17

Fix  for PackageTask
The PackageTask should run for all platforms that are _not_ of type JVM.
e574657	introduced this mistake when using the isJVM method instead of
the equality check.

src/defrac/intellij/compiler/PackageTask.java
@@ -39,7 +39,7 @@ public final class PackageTask extends BooleanBasedCompilerTask {
 
   @Override
   protected boolean shouldRunForFacet(@NotNull final DefracFacet facet) {
-    return facet.getPlatform().isJVM();
+    return !facet.getPlatform().isJVM();
   }
 
   @NotNull